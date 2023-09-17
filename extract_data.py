7c65f0f11a636c2b06596be0267a8982a3af260f
Anjali Bhargava <bhargavaav@vt.edu>
2023-09-17 00:21:28 -0400

    checked valid/invalid urls

--- extract_data.py
+++ extract_data.py
@@ -1,6 +1,10 @@
 from pyzotero import zotero
 from copy import deepcopy
 from typing import Any, List
+import requests
+import smtplib
+from email.mime.text import MIMEText
+
 
 class ZoteroWrapper:
     def __init__(self, user_account_id: int, user_type: str, access_token: str):
@@ -20,7 +24,22 @@ class ZoteroWrapper:
         larger_items = [deepcopy(i) for _ in range(int(multiplier)) for i in items]
         return larger_items
     #check validity of link
-
+    def is_url_valid(self, url: str) -> bool:
+        headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+        try:
+            response = requests.get(url, headers=headers, timeout=10)  # Increase timeout to 10 seconds
+            # Checking if status_code is between 200 and 299 which are typically considered successful
+            return 200 <= response.status_code < 300
+        except requests.RequestException:
+            return False
+    def check_links_validity(self, items: List[Any]) -> List[bool]:
+        """
+        Returns a list of boolean values indicating whether each URL in the items is valid or not.
+        """
+        return [self.is_url_valid(item['data']['url']) for item in items]
+    
 USER_ID = 12354040
 USER_TYPE = 'user'
 ACCESS_TOKEN = '9sPrZSh9r4VKeF7zO8Aau8qI'
@@ -32,10 +51,22 @@ z = ZoteroWrapper(
 )
 
 items = z.query()
+dummy_item = {
+    'data': {
+        'title': 'Dummy Title',
+        'url': 'http://www.invalidlink1234567890.com',
+        'tags': [{'tag': 'Dummy Tag'}]
+    }
+}
+
+items.append(dummy_item)
+validities = z.check_links_validity(items)
+
 # items = z.duplicate_data(items, 1e4)
 
 # we've retrieved the latest five top-level items in our library
 # we can print each item's item type and ID
-for item in items:
+for item, valid in zip(items, validities):
     tag_names = [tag['tag'] for tag in item['data']['tags']]
-    print('Title: %s | URL: %s| Tags: %s' % (item['data']['title'], item['data']['url'], ', '.join(tag_names)))
+    status = "Valid" if valid else "Invalid"
+    print('Title: %s | URL: %s| Status: %s| Tags: %s' % (item['data']['title'], item['data']['url'], status, ', '.join(tag_names)))
